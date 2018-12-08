aee11c0db48cb957dba16b5e0af32bae8384ab47
Eugene-Metlitski <evguenimetlitski@outlook.com>
2018-12-08 23:49:30 +1000

    added html % css for large screen

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ProfilePictureComponent } from './components/profile-picture/profile-picture.component';
+import { HamburgerButtonComponent } from './components/hamburger-button/hamburger-button.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NavComponent,
+    ProfilePictureComponent,
+    HamburgerButtonComponent,
+    SidebarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
