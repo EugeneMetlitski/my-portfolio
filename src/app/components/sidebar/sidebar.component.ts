5b47bebd6cf4d69510b0499d152f282a9ae6b737
Eugene-Metlitski <evguenimetlitski@outlook.com>
2018-12-20 13:37:39 +1000

    Sidebar updated

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { SidebarStateService } from './services/sidebar-state.service';
 import { SidebarContentService } from './services/sidebar-content.service';
 
@@ -7,12 +7,12 @@ import { SidebarContentService } from './services/sidebar-content.service';
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent implements AfterViewInit {
+export class SidebarComponent {
   //#region variables
 
-  private w: number; // Width of sidebar based on content
-  private width;     // The element for rendering width
-  private left;      // Background element slides when hidden
+  @ViewChild('bg') bg: ElementRef; // Background element
+  @ViewChild('rw') rw: ElementRef; // Render-Width element
+  @ViewChild('wl') wl: ElementRef; // Wall element
 
   //#endregion
   //#region init
@@ -20,50 +20,74 @@ export class SidebarComponent implements AfterViewInit {
   constructor(
     private stateService: SidebarStateService,
     private contentService: SidebarContentService
-  ) {}
-
-  ngAfterViewInit() {
-    this.setupVariables();
+  ) {
     this.setupSateService();
   }
 
   //#endregion
   //#region getters & setters
 
+  /**
+   * The title of sidebar.
+   */
   get title() { return `Projects`; }
+
+  /**
+   * The width of sidebar is determined by the content loaded
+   * to the sidebar (i.e. how wide headings and links are).
+   */
+  private get w() { return document
+      .getElementsByClassName('sidebar')[0]
+      .getBoundingClientRect().width;
+  }
+
+  /**
+   * Set the style left for the background element of the
+   * sidebar. This moves sidebar, vertical line and sidebar's
+   * background.
+   */
+  private set left(arg: string) {
+    this.bg.nativeElement.style.left = arg;
+  }
+
+  /**
+   * Set the rendered width for the sidebar (i.e. how much
+   * width space will sidebar take up in document flow).
+   */
+  private set width(arg: string) {
+    this.rw.nativeElement.style.width = arg;
+  }
+
+  /**
+   * Set the width of the wall. Wall is the element behind
+   * which sidebar hides when it is in hidden state.
+   */
+  private set wall(arg: string) {
+    this.wl.nativeElement.style.width = arg;
+  }
 
   //#endregion
   //#region private-functions
 
-  private setupVariables() {
-    // Get the width of sidebar based on it's content
-    this.w = document
-      .getElementsByClassName('sidebar')[0]
-      .getBoundingClientRect().width;
-
-    this.width = document.getElementById('width');
-    this.left = document.getElementById('background');
-    const wall = document.getElementById('wall');
-
-    this.width.style.width = `${this.w + 20}px`;
-    wall.style.width = `${this.w + 40}px`;
-  }
-
+  /**
+   * Subscribe to state service and provide a function to
+   * be called when the state of sidebar changes.
+   */
   private setupSateService = () => {
 
-    // Provide function to call when hanburger button clicked
+    // Provide function to call when sidebar-button is clicked
     this.stateService.subscribe((renderWidth: boolean) => {
-      const hide = !this.stateService.visible;
 
-      if (hide) {
-        this.width.style.width = `0`;
-        this.left.style.left = `-${this.w + 40}px`;
+      // If the sidebar is in visible state
+      if (this.stateService.visible) {
+        this.width = (renderWidth) ? `${this.w + 20}px` : `0`;
+        this.left = `0`;
       } else {
-        (renderWidth) ?
-          this.width.style.width = `${this.w + 20}px` :
-          this.width.style.width = `0`;
-        this.left.style.left = `0`;
+        this.width = `0`;
+        this.left = `-${this.w + 40}px`;
       }
+
+      this.wall = `${this.w + 40}px`;
     });
   }
 
